9188ab576ea49081266688ba4e699e4bb38bfbfc
GaurieeSL <gaurilohote6899@gmail.com>
2026-03-24 12:12:23 +0530

    Update hand_tracking.py

--- gesture_detection/hand_tracking.py
+++ gesture_detection/hand_tracking.py
@@ -1,7 +1,12 @@
 import cv2
 import mediapipe as mp
+import serial
+import time
 
-# Initialize MediaPipe
+# CHANGE THIS PORT
+ser = serial.Serial('COM3', 115200)
+time.sleep(2)
+
 mp_hands = mp.solutions.hands
 hands = mp_hands.Hands(max_num_hands=1)
 mp_draw = mp.solutions.drawing_utils
@@ -12,14 +17,9 @@ tip_ids = [4, 8, 12, 16, 20]
 
 while True:
     success, img = cap.read()
-
-    if not success:
-        print("Camera error")
-        break
-
     img = cv2.flip(img, 1)
-    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
 
     if results.multi_hand_landmarks:
@@ -28,22 +28,21 @@ while True:
             mp_draw.draw_landmarks(img, handLms, mp_hands.HAND_CONNECTIONS)
 
             lmList = []
-
             for id, lm in enumerate(handLms.landmark):
                 h, w, c = img.shape
                 cx, cy = int(lm.x * w), int(lm.y * h)
                 lmList.append((cx, cy))
 
-            if len(lmList) != 0:
+            if lmList:
                 fingers = []
 
-                # Thumb (try both directions)
+                # Thumb
                 if lmList[4][0] > lmList[3][0]:
                     fingers.append(1)
                 else:
                     fingers.append(0)
 
-                # Fingers (index to pinky)
+                # Other fingers
                 for id in range(1, 5):
                     if lmList[tip_ids[id]][1] < lmList[tip_ids[id] - 2][1]:
                         fingers.append(1)
@@ -52,17 +51,13 @@ while True:
 
                 total_fingers = fingers.count(1)
 
-                # Debug print (important)
-                print("Fingers:", total_fingers)
+                print("Sending:", total_fingers)
 
-                # Display on screen
+                # SEND TO ESP32
+                ser.write(f"{total_fingers}\n".encode())
+
                 cv2.putText(img, f'Fingers: {total_fingers}', (20, 80),
                             cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 255, 0), 3)
-
-    else:
-        # No hand detected
-        cv2.putText(img, 'No Hand', (20, 80),
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, (0, 0, 255), 3)
 
     cv2.imshow("Gesture Control", img)
 
@@ -71,3 +66,4 @@ while True:
 
 cap.release()
 cv2.destroyAllWindows()
+ser.close()
